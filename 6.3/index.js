8bea701caa026ebfbf1f0e90892ab53f7a149145
Max Lutsenko <lutsenko.maxim@gmail.com>
2018-12-03 22:42:10 +0200

    Prime number with arrays

--- 6.3/index.js
+++ 6.3/index.js
@@ -3,15 +3,24 @@ console.log("Ready!");
 // 1. isPrime - Returns true or false, indicating whether the given number is prime.
 
 function isPrime (x) {
+  var arr =[];
+  var integerArray = [];
 
-    for (var i = 1; i<= x; i++) {
-      if ( x % i !== 0 ) {
-        console.log("true")
-      } else {
-        console.log("false")
-      }
+  for (var i = 0, y = 1; i <= x ; i++, y++) {
 
+    arr[i] = x / y;
+
+    if ( Number.isInteger(arr[i]) === true ) {
+      integerArray.push(arr[i]);
     }
+
+  }
+
+  if ( 2 < integerArray.length ) {
+    return false
+  } else {
+    return true
+  }
 
 }
 
